1fbe7e17ef341f2351d9dfbc55c9dab407daa989
Max Tilden <maxtilden@gmail.com>
2021-08-18 12:59:20 -0700

    assignment complete

--- index.js
+++ index.js
@@ -39,12 +39,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name =name;
+  this.age = age;
+  this.stomach = [];
 }
-
-
-
+Person.prototype.eat = function (edible) {
+      if(this.stomach.length < 10){
+        this.stomach.push(edible);
+      }
+};
+Person.prototype.poop = function(){
+    this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 
 
@@ -63,9 +73,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+};
 
 
 /*
@@ -75,18 +91,22 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name, age);
+ this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`;
+};
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Whenver a function is contained in the global scope the value of this will be the the window object because it has nowhere else to pull from.
+  2. Implicit binding is where dot notation is used then this refers to whatever function is put to the left of the dot when the funtion is called.
+  3. using methods like call and apply we can use explicit binding to tell the funtion ecactly what funtion to use.
+  4. New binding is used to make new objects using constructer funtions and caused 'this' to refer to the new object that is created.
 */
 
 
